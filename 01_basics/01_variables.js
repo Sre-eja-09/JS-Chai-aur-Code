66d839ddd7b28a13f7511e400fa7f962df8f5a64
Sreeja Prakash <104756330+Sre-eja-09@users.noreply.github.com>
2023-12-30 07:11:40 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId =144356
+let accountEmail = "sreeja@gmail.com"
+var accountPassword = "12345"
+accountCity ="jaipur"
+accountEmail="sree@gmail.com"
+accountPassword="23245"
+accountCity ="bangaluru"
+//accountId=2 //not allowed
+//prefer not to use var because of the issue of block scope and functional scope
+console.log(accountId);
+console.table([accountPassword,accountEmail,accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict";// treat all the code as newer version of JS
+//javascript ka engine browser ke andar hota h 
+//alert(3+3) // this syntax won't work as we are using nodejs and not browser
+let name="hitesh"
+let age=18
+let isLoggedin=false
+let state;
+console.log(typeof age);
+// null is an object (type)
+// undefined is undefined(type)
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
