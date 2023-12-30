301124d6dbf1a5a51dca1c348400f880be78ac41
Sreeja Prakash <104756330+Sre-eja-09@users.noreply.github.com>
2023-12-30 12:55:35 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,22 +1,34 @@
-//let score = "33abv"; //when we print this value it will be NaN not a number but its type would be string and after coverting into number it becomes of type number
-//let score = null; when we print this value it will be 0 and type is object and after converting it into number the type becomes number
-//let score = undefined; //when we print this value it will be NaN not a number but its type would be undefined and after coverting into number it becomes of type number
-let score = true; //when we print this value it will be 1 for true and 0 for false but its type would be boolean and on conversion to number the type becomes number
-console.log(typeof score);
-console.log(typeof(score));
+// //let score = "33abv"; //when we print this value it will be NaN not a number but its type would be string and after coverting into number it becomes of type number
+// //let score = null; when we print this value it will be 0 and type is object and after converting it into number the type becomes number
+// //let score = undefined; //when we print this value it will be NaN not a number but its type would be undefined and after coverting into number it becomes of type number
+// let score = true; //when we print this value it will be 1 for true and 0 for false but its type would be boolean and on conversion to number the type becomes number
+// console.log(typeof score);
+// console.log(typeof(score));
 
-//conversion
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// //conversion
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
-//let isLoggedIn =1;//true
-//let isLoggedIn ="";//empty strings gives false
-let isLoggedIn ="Sreeja"; //any string value will give true
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// //let isLoggedIn =1;//true
+// //let isLoggedIn ="";//empty strings gives false
+// let isLoggedIn ="Sreeja"; //any string value will give true
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
 
-let someNumber = 33;
-let stringNumber =String(someNumber);
-console.log(someNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33;
+// let stringNumber =String(someNumber);
+// console.log(someNumber);
+// console.log(typeof stringNumber);
+
+// **************Operations***************
+let value =3;
+let negValue = -value;
+//console.log(negValue);
+
+// console.log(1+"2");
+// console.log("1"+2);
+// console.log("1"+2+2);//here the first number is a string hence all the other numbers are also treated as strings
+// console.log(1+2+"2");//here the first number is a number hence the first 2 numbers are added and the last number is string which is concatenated
+console.log(+true); // 1
+console.log(+""); //0
---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,9 @@
+// console.log(null >= 0);//true
+// console.log(null == 0);//false
+// console.log(null > 0);//false
+
+// console.log(undefined == 0);//false
+// console.log(undefined < 0);//false  ye true hona chahiye pr false aarha h
+// console.log(undefined > 0);//false
+
+console.log("2" === 2); // it does not convert one datatype into another it compares the data types also and then returns the output accordingly
