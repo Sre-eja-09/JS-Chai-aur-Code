6703eea90e568085224bbac9ead7d65892b49f0d
Sreeja Prakash <104756330+Sre-eja-09@users.noreply.github.com>
2024-01-06 18:50:08 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,18 @@
+function saymyname(){
+    console.log("S")
+    console.log("R")
+    console.log("E")
+    console.log("E")
+}
+saymyname()
+//  function addTwoNumbers(number1,number2){ //parameters
+//        console.log(number1+number2)
+//  }
+function addTwoNumbers(number1,number2){ //parameters
+    let result = number1+number2
+    return result
+    //console.log(number1+number2)
+}
+//addTwoNumbers(1,2) //arguments
+const result = addTwoNumbers(2,3)
+console.log("result:",result) //undefined , but why?
