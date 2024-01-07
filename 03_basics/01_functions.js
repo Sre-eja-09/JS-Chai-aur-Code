476adc1f1e4741a13b3ef1825c37d277b0db3246
Sreeja Prakash <104756330+Sre-eja-09@users.noreply.github.com>
2024-01-07 09:59:04 +0000

    funcs

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -9,10 +9,16 @@ saymyname()
 //        console.log(number1+number2)
 //  }
 function addTwoNumbers(number1,number2){ //parameters
-    let result = number1+number2
-    return result
-    //console.log(number1+number2)
+    // let result = number1+number2
+   // console.log(number1+number2)
+    return number1+number2
+    
 }
 //addTwoNumbers(1,2) //arguments
 const result = addTwoNumbers(2,3)
-console.log("result:",result) //undefined , but why?
+//console.log("result:",result) //undefined , but why?
+
+function loginUser(username){
+   return `${username} just loggedin`
+}
+console.log(loginUser("Sreeja"))
